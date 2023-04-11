96645d0ea32e6c7343d664c8244d4f57b52796c0
JonasDELLOUH <jdellouh1@gmail.com>
2023-04-11 17:59:46 +0100

    app v1 ok1

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./styles.css";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
+import Forecast from "./components/forecast/forecast";
+
+export default function App() {
+
+  const [currentWeather, setCurrentWeather] = useState();
+  const [forecast, setForecast] = useState();
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split("");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const forecastFtech = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    Promise.all([currentWeatherFetch, forecastFtech]).then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forecastResponse = await response[1].json();
+
+      setCurrentWeather({city: searchData.label ,...weatherResponse});
+      setForecast({city: searchData.label, ...forecastResponse});
+    }).catch((err) => console.log(err))
+
+  }
+
+  console.log(forecast);
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
+      {currentWeather && <CurrentWeather data={currentWeather   } />}
+      {forecast && <Forecast data={forecast} />}
+    </div>
+  );
+}
---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '497b48de3fmshf50c24d07d24ae6p1fd43fjsn500952a7454c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9ec26366fe7b4c53dec8cda6007a8b68";
